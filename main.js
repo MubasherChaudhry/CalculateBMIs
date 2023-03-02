2dc8000967a15bbacb3793de58c0861a2c5bb006
Mubasher <mobi_20_11@yahoo.com>
2023-03-02 20:47:53 +0100

    Exercise 1 is solved

--- main.js
+++ main.js
@@ -12,7 +12,13 @@ const heightMack = 1.69;
 const massMobi = 95;
 const heightMobi = 1.88;
 
-const BMIMack= massMack/heightMack**2
-const BMIMobi= massMobi/heightMobi**2
+const BMIMack = massMack / heightMack ** 2;
+const BMIMobi = massMobi / (heightMobi * heightMobi);
 
 console.log(BMIMack, BMIMobi);
+
+const MackHigherBMI = BMIMack > BMIMobi;
+
+console.log( BMIMack,BMIMobi, MackHigherBMI );
+//27.309968138370508 26.87867813490267 true
+
